51b5f45e186beffecc3832d0635ea9224b59ea89
Muryllo <muryllopimenta@gmail.com>
2021-10-04 03:16:46 -0300

    :sparkles: feature: Added a Polyfill to support both Browser and Node.

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,11 @@
-export { WebSocket } from 'ws';
+//Simple polyfill for Node.js/Browser.
+
+import { WebSocket as NodeW3CWebSocket } from 'ws';
+
+var W3CWebSocket = NodeW3CWebSocket;
+
+if (!!WebSocket && (typeof WebSocket != 'undefined')){
+  W3CWebSocket = <any>window.WebSocket;
+}
+
+export { W3CWebSocket as WebSocket };
